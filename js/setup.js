afabc9e31f2c0397a31964e948ff459b8e4e5aa2
k5va <k5va@yandex.ru>
2019-06-08 20:15:09 +0300

    added showSetupBlock function

---
+++ js/setup.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Shows setup block
+ */
+var showSetupBlock = function () {
+  var setupBlock = document.querySelector('.setup');
+  setupBlock.classList.remove('hidden');
+};
+
+showSetupBlock();
